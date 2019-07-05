44d6fd8f48b85f23ad2ccea210c33c3859a0f027
Joshua Pollak <abottomlesspit@gmail.com>
2019-07-04 22:56:34 -0700

    working demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,43 @@ import SlideGrid from '@pyrogenic/slide-grid/lib/SlideGrid';
 
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <SlideGrid>
+interface IAppState {
+  tiles: string[];
+}
 
-    </SlideGrid>
-  );
+class App extends React.Component<{}, IAppState> {
+  constructor(props: {}) {
+    super(props);
+    const tiles = [" "];
+    for (let i = 1; i <= 15; ++i) {
+      tiles.push(i.toString());
+    }
+    this.state = { tiles };
+  }
+
+  public render = () => <SlideGrid exchange={this.exchange}>
+    {this.state.tiles.map((tile) => <div key={tile} id={tile}>{tile}</div>)}
+  </SlideGrid>;
+
+  private exchange = (a: string, b: string) => {
+    this.setState((state) => {
+      const ai = state.tiles.indexOf(a);
+      const bi = state.tiles.indexOf(b);
+      const tiles: string[] = [];
+      console.log({a, ai, b, bi});
+      state.tiles.forEach((tile, i) => {
+        if (i === ai) {
+          tiles[bi] = tile;
+        } else if (i === bi) {
+          tiles[ai] = tile;
+        } else {
+          tiles[i] = tile;
+        }
+      });
+      return { tiles };
+    })
+  }
+
 }
 
 export default App;
